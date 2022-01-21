abb6e3940ca9db7033254770f886771f4038b373
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2022-01-21 09:15:51 -0300

    [add] telas referente ao adminator

---
+++ src/main/resources/src/emissor-fiscal_static/static/js/validacoes.js
@@ -0,0 +1,28 @@
+
+//O txt, que tiver essa class, irá PERMITIR apenas LETRAS e ESPAÇOS
+$(".somenteLetras").on("input", function(){
+    var regexp = /[^a-zA-Z\u00C0-\u00FF ]/g;
+    if(this.value.match(regexp)){
+      $(this).val(this.value.replace(regexp,''));
+    }
+});
+
+//O txt, que tiver essa class, irá PERMITIR apenas NÚMEROS
+$(".somenteNumeros").on("input", function(){
+  var regexp = /[^0-9]/g;
+  if(this.value.match(regexp)){
+    $(this).val(this.value.replace(regexp,''));
+  }
+});
+
+//O txt, que tiver essa class, irá PERMITIR apenas NÚMEROS, E irá trocar a ", VIRGULA" por ponto
+$(".somenteNumerosPontoEVirgula").on("input", function(){
+    var regexp = /[^0-9,.]/g;
+    if(this.value.match(regexp)){
+      $(this).val(this.value.replace(regexp,''));
+    }
+    $(this).val(this.value.replace(",", "."));
+//    this.value.replace(",", ".");
+});
+
+
