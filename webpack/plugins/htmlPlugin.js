8b5018f172dbf3c01604cbbeb3cc79b256df3c20
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2022-01-27 17:43:09 -0300

    [fix] arrumando forms no novo layout

--- webpack/plugins/htmlPlugin.js
+++ webpack/plugins/htmlPlugin.js
@@ -7,9 +7,30 @@ const titles = {
   'index': 'Dashboard',
   // '/emissor-fiscal_html/default' : 'default',
   '/default' : 'default',
+
   '/cfop/cadastro' : 'Cadastro de Cfop',
   '/cfop/consulta': 'Consulta de Cfop',
-  
+
+  '/ncm/cadastro' : 'Cadastro de NCM',
+  '/ncm/consulta': 'Consulta de NCM',
+  '/ncm/modal-add-ncm' : 'Modal add ncm',
+
+  '/estado/cadastro' : 'Cadastro de Estado',
+  '/estado/consulta': 'Consulta de Estado',
+
+  '/operacao/cadastro' : 'Cadastro de Operação',
+  '/operacao/consulta': 'Consulta de Operação',
+  '/operacao/modal-deletar': 'modal-deletar',
+
+  '/tributacao-estadual/cadastro' : 'Cadastro de ICMS',
+  '/tributacao-estadual/consulta': 'Consulta de ICMS',
+
+  '/tributacao-estadual-guia/cadastro' : 'Cadastro de tributação estadual - (GUIA)',
+  '/tributacao-estadual-guia/consulta': 'Consulta de tributação estadual - (GUIA) ',
+  '/tributacao-estadual-guia/email-calculo-icms-st-entrada' : 'email-calculo-icms-st-entrada',
+
+  '/tributacao-federal/cadastro' : 'Cadastro de tributação federal',
+  '/tributacao-federal/consulta': 'Consulta de tributação federal',
   'blank': 'Blank',
   // 'buttons': 'Buttons',
   // 'calendar': 'Calendar',
